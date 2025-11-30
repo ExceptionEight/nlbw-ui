f897d1960ccb88e56ccfdd746dac2efefcdd293b
ExceptionEight <cyanogen500@gmail.com>
2025-11-30 22:52:25 +0300

    fix: prevent page from scrolling to top when modal is opened

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -38,18 +38,18 @@ function Dashboard({ dateRange }) {
 
   useEffect(() => {
     if (modalVisible) {
+      const scrollY = window.scrollY
       document.body.style.overflow = 'hidden'
       document.body.style.position = 'fixed'
+      document.body.style.top = `-${scrollY}px`
       document.body.style.width = '100%'
-    } else {
-      document.body.style.overflow = ''
-      document.body.style.position = ''
-      document.body.style.width = ''
-    }
-    return () => {
-      document.body.style.overflow = ''
-      document.body.style.position = ''
-      document.body.style.width = ''
+      return () => {
+        document.body.style.overflow = ''
+        document.body.style.position = ''
+        document.body.style.top = ''
+        document.body.style.width = ''
+        window.scrollTo(0, scrollY)
+      }
     }
   }, [modalVisible])
 
